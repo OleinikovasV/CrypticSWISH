655faa2fc57e5ea9f8dae55d4f6477bf4b84c836
OleinikovasV <v.oleinikovas@gmail.com>
2018-01-12 16:28:13 +0000

    tcl template scripts to calc volmaps with VMD

---
+++ volmaps/calc_volmap_1JWP+BEN_SWISH.tcl
@@ -0,0 +1,28 @@
+# load molecules
+mol new {/home/vladas/Dropbox/IPython_notebooks/JACS_2016/TEM1/ref_1JWP+WAT+BEN.gro} type {gro} first 0 last -1 step 1 waitfor 1
+# remove first frame
+animate delete  beg 0 end 0 skip 0
+
+
+## LOOP 1! different trajs
+for {set i 0} {$i <= 7} {incr i} {
+
+set trajfile /home/vladas/mnt/IB-server/storage/project_UCB/TEM1/HREX+FRAGS/TEM1+BEN/traj_full_fixed/traj${i}_aligned_full.xtc
+
+
+## LOOP 2! divide traj blocks
+for {set x 0} {$x <= 4} {incr x} {
+
+set begin [expr $x * 1000 ]
+set end [expr $x * 1000 + 1000 ]
+set outfile density-DM_1JWP+BEN_SWISH-traj${i}_frames-$begin-$end.dx
+
+# load traj chunk!
+mol addfile $trajfile type {xtc} first $begin last $end step 1 waitfor -1
+# do density calculation!
+volmap density [atomselect top "name DM"] -res 1.0 -radscale 5.0 -allframes -combine avg -o $outfile -minmax {{-45 -60 -25} {65 60 95}} -checkpoint 0
+# remove coordinates
+animate delete  beg 0 end -1 skip 0
+
+}
+}
